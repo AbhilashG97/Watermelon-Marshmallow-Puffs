5e301f668f3642a756ccdaf7e4bcacc17fe09c64
AbhilashG97 <abhilash.ag1997@gmail.com>
2019-10-16 21:05:32 +0530

    added Spring Boot Project :star:

---
+++ src/main/java/edu/amrita/medishop/MediShopApplication.java
@@ -0,0 +1,12 @@
+package edu.amrita.medishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediShopApplication.class, args);
+	}
+}
